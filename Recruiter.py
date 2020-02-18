85cbdb3352c5fd9a85811698fd16333de168992e
D. Kanter Eivin <kanter.dev@gmail.com>
2020-02-18 09:15:56 -0500

    Added Documentation

--- Recruiter.py
+++ Recruiter.py
@@ -2,37 +2,50 @@ import json
 import sqlite3
 import time
 from datetime import datetime, timedelta
+from typing import List, Dict, Any
 
 import requests
 
 
 class Recruiter:
     def __init__(self, settings=None, settings_path="settings.json"):
+        """ Loads recruiter settings and connects to the sqlite3 database specified in the settings.
+        :param settings=None A dictionary containing all settings to be used by the recruiter.
+        :param settings_path="settings.json" The filepath to locate settings, if settings param is not provided.
+        """
         if settings:  # a settings object has been provided
-            self.settings: dict = settings
+            self.settings: Dict[str, Any] = settings
         else:
             with open(settings_path) as settings:  # load and encode settings to object
                 self.settings = json.load(settings)
         try:
-            self.db = sqlite3.connect(self.settings['sec']['db_path'])
+            self.db: Any = sqlite3.connect(self.settings['sec']['db_path'])
             time_test: str = self.db.execute("SELECT datetime('now');").fetchone()[0]
             print("Successful connection to the database at {}.".format(time_test))
         except:
             raise Exception("Unable to connect to the SQLLite Database.")
 
     def run_recruitment_round(self) -> None:
-        current_time = datetime.today()
+        """ Run a single recruitment round, fetches all nations from the Nations API endpoints, iterates them,
+        and sends a message if the nation meets the self.should_contact(nation) conditions.
+        :return: None
+        """
+        current_time: datetime = datetime.today()
         print("Recruitment Round Starting @ {}".format(current_time))
 
         # Fetch and filter nations to those which should be contacted
-        all_nations: list = self.get_nations()
+        all_nations: List[Dict[str, Any]] = self.get_nations()
         print("<-- Nations API (Fetched Nations from PnW)")
         target_nations: iter = filter(self.should_contact, all_nations)
 
-        conn = self.login()
+        conn: requests.Session = self.login()
+        failed: int = 0  # number of failed requests
 
         for nation in target_nations:
-            self.send_message(nation['leader'], conn)
+            try:
+                self.send_message(nation['leader'], conn)
+            except Exception:
+                failed += 1
             self.db.execute("INSERT OR REPLACE INTO nations_contacted(nation_id, time_sent) " +
                             "VALUES (?, datetime('now'));",
                             (int(nation['nationid']),))
@@ -40,7 +53,9 @@ class Recruiter:
             print(f"--> Message Sent to {nation['leader']} (id: {nation['nationid']} @ {datetime.today()}")
 
     def login(self) -> requests.Session:
-        # Login to PnW and create new session.
+        """ Authenticates with login details specified in settings.json and returns a logged in requests session.
+        :return: The logged in session -- to be used for all outgoing traffic (POST requests to send messages).
+        """
         payload = {
             'email': self.settings['sec']['user'],
             'password': self.settings['sec']['pass'],
@@ -62,8 +77,16 @@ class Recruiter:
         }
         conn.post(self.settings['READ_ONLY']['msg'], payload)
 
-    # determines whether or not the nation id should be contacted
     def should_contact(self, nation: dict) -> bool:
+        """ Checks the specified nation for whether it should be contacted, based on:
+            - alliance
+            - city count
+            - inactive time
+            - manual exclusions
+            - previously contacted nations from the database
+        :param nation: The nation for which to check if it should be contacted.
+        :return:
+        """
         if str(nation['alliance']) not in self.settings['info']['target_alliance']:  # ["None"] for no alliance
             return False
         if int(nation['cities']) < self.settings['info']['min_cities']:  # too few cities
@@ -84,9 +107,11 @@ class Recruiter:
 
         return True
 
-    def get_nations(self) -> list:
+    def get_nations(self) -> List[Dict[str, Any]]:
+        """ Fetches all nations from the nations API and returns them as a list of parsed JSON dictionaries.
+        :return:
+        """
         endpoint: str = self.settings['READ_ONLY']['nations'] + self.settings['sec']['api_key']
-        # GET from NationAPI endpoint
         data: dict = requests.get(endpoint).json()
         return data['nations']
 
